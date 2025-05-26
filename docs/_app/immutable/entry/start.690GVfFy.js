38ed00dbfe236bccdc40b905aa626ebd8fca775b
Tyra Tinglev <tyrtin03@elevad.umea.se>
2025-05-26 14:31:10 +0200

    message

---
+++ docs/_app/immutable/entry/start.690GVfFy.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/X9Cok652.js";export{o as load_css,r as start};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,7 @@ import { skeleton } from '@skeletonlabs/tw-plugin'
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}','./docs/*.html' , join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],			
-	theme: {
+	content: ['./src/**/*.{html,js,svelte,ts}','./docs/*.html' , join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],				theme: {
 		extend: {},
 	},
 	plugins: [
